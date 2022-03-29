6d5ae3ae4e735714aac231d9b41faec70e509554
sinryuji <siler2474@gmail.com>
2022-03-29 23:43:38 +0900

    fix: Madatory gnlTester All OK

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,41 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 23:36:51 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/03/28 19:01:24 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/03/29 23:42:22 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*cut_line(char **save)
+char	*get_line(char **save)
 {
 	char	*ret;
-	int		i;
 	char	*tmp;
+	int		i;
 
 	i = 0;
 	if (!**save)
 	{
 		free(*save);
+		*save = NULL;
 		return (NULL);
 	}
-	while (**(save + i) && **(save + i) != '\n')
+	while (*(*save + i) && *(*save + i) != '\n')
 		i++;
 	ret = ft_substr(*save, 0, i + 1);
-	tmp = ft_substr(*save, i + 2, ft_strlen(*save));
+	tmp = ft_substr(*save, i + 1, ft_strlen(*save) - ft_strlen(ret));
 	free(*save);
 	*save = tmp;
 	return (ret);
 }
 
-char	*read_line(int fd, char **save, char *buf)
+char	*read_line(int fd, char *save, char *buf)
 {
-	char	*tmp;
 	ssize_t	read_byte;
+	char	*tmp;
 
 	read_byte = 1;
-	while (!(ft_strchr(*save, '\n')) && read_byte != 0)
+	while (!(ft_strchr(save, '\n')) && read_byte != 0)
 	{	
 		read_byte = read(fd, buf, BUFFER_SIZE);
 		if (read_byte == -1)
@@ -48,15 +49,15 @@ char	*read_line(int fd, char **save, char *buf)
 			return (NULL);
 		}
 		buf[read_byte] = 0;
-		if (!*save)
+		if (!save)
 			tmp = ft_strdup(buf);
 		else
-			tmp = ft_strjoin(*save, buf);
-		free(*save);
-		*save = tmp;
+			tmp = ft_strjoin(save, buf);
+		free(save);
+		save = tmp;
 	}
 	free(buf);
-	return (cut_line(save));
+	return (save);
 }
 
 char	*get_next_line(int fd)
@@ -69,18 +70,8 @@ char	*get_next_line(int fd)
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	return (read_line(fd, &save, buf));
-}
-
-#include <stdio.h>
-#include <fcntl.h>
-
-int	main()
-{
-	int	fd;
-
-	fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	//while(1);
+	save = read_line(fd, save, buf);
+	if (!save)
+		return (NULL);
+	return (get_line(&save));
 }
